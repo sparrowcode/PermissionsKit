1b624c8852e16624f09b0dae86d261a9bf0137c3
Nicolas Degen <ndegen@me.com>
2019-10-27 09:49:03 +0100

    update ios

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SPPermission",
-    platforms: [.iOS(.v9)],
+    platforms: [.iOS(.v10)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
