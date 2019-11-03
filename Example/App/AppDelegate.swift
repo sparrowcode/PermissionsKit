97f4d092e77dac73742e07921be871a28b9d1eb1
Ivan Vorobei <hello@ivanvorobei.io>
2019-11-03 12:34:51 +0300

    Add new struct files

---
+++ Example/App/AppDelegate.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
