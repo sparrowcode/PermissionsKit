dbab8bb2db229e824a4815471c7328d434a5ff8e
Ivan Vorobei <hello@ivanvorobei.io>
2019-11-03 19:28:45 +0300

    Add Permissions
    Also rename some enums.

---
+++ Source/Handlers/SPRequestPermissionLocationHandler.swift
@@ -0,0 +1,142 @@
+// The MIT License (MIT)
+// Copyright © 2017 Ivan Vorobei (hello@ivanvorobei.by)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Foundation
+import MapKit
+
+class SPPermissionAlwaysAuthorizationLocationHandler: NSObject, CLLocationManagerDelegate {
+    
+    static var shared: SPPermissionAlwaysAuthorizationLocationHandler?
+    
+    lazy var locationManager: CLLocationManager =  {
+        return CLLocationManager()
+    }()
+    
+    var completionHandler: SPPermissionAuthorizationHandlerCompletionBlock?
+    
+    override init() {
+        super.init()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("locationManager \(status)")
+        
+        if status == .notDetermined {
+            print("2")
+            return
+        }
+
+        if let completionHandler = completionHandler {
+            completionHandler(self.isAuthorized)
+        }
+    }
+    
+    private var whenInUseNotRealChangeStatus: Bool = false
+    
+    func requestPermission(_ completionHandler: @escaping SPPermissionAuthorizationHandlerCompletionBlock) {
+        self.completionHandler = completionHandler
+        
+        let status = CLLocationManager.authorizationStatus()
+        
+        switch status {
+        case .notDetermined:
+            locationManager.delegate = self
+            locationManager.requestAlwaysAuthorization()
+        case .authorizedWhenInUse:
+            locationManager.delegate = self
+            locationManager.requestAlwaysAuthorization()
+            self.whenInUseNotRealChangeStatus = true
+        default:
+            completionHandler(self.isAuthorized)
+        }
+    }
+    
+    var isAuthorized: Bool {
+        let status = CLLocationManager.authorizationStatus()
+        if status == .authorizedAlways {
+            return true
+        }
+        return false
+    }
+    
+    deinit {
+        locationManager.delegate = nil
+    }
+}
+
+class SPPermissionWhenInUseAuthorizationLocationHandler: NSObject, CLLocationManagerDelegate {
+    
+    static var shared: SPPermissionWhenInUseAuthorizationLocationHandler?
+    
+    lazy var locationManager: CLLocationManager =  {
+        return CLLocationManager()
+    }()
+    
+    var completionHandler: SPPermissionAuthorizationHandlerCompletionBlock?
+    
+    override init() {
+        super.init()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .notDetermined {
+            return
+        }
+        
+        if let completionHandler = completionHandler {
+            completionHandler(self.isAuthorized)
+        }
+    }
+    
+    func requestPermission(_ completionHandler: @escaping SPPermissionAuthorizationHandlerCompletionBlock) {
+        self.completionHandler = completionHandler
+        
+        let status = CLLocationManager.authorizationStatus()
+        if (status == .notDetermined) || (status == .authorizedAlways) {
+            locationManager.delegate = self
+            locationManager.requestWhenInUseAuthorization()
+        } else {
+            completionHandler(self.isAuthorized)
+        }
+    }
+    
+    var isAuthorized: Bool {
+        let status = CLLocationManager.authorizationStatus()
+        if status == .authorizedWhenInUse {
+            return true
+        }
+        return false
+    }
+    
+    deinit {
+        locationManager.delegate = nil
+    }
+}
+
+extension SPPermissionAlwaysAuthorizationLocationHandler {
+    
+    typealias SPPermissionAuthorizationHandlerCompletionBlock = (Bool) -> Void
+}
+
+extension SPPermissionWhenInUseAuthorizationLocationHandler {
+    
+    typealias SPPermissionAuthorizationHandlerCompletionBlock = (Bool) -> Void
+}
--- Source/SPPermission.swift
+++ Source/SPPermission.swift
@@ -1,15 +1,160 @@
+// The MIT License (MIT)
+// Copyright © 2017 Ivan Vorobei (hello@ivanvorobei.by)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 import UIKit
 
-public enum SPPermission {
+public enum SPPermission: String {
     
-    public static func request(mode: SPPermissionMode, permissions: [SPPermissionType]) {
-        
+    case speech = "speech"
+    case motion = "motion"
+    case camera = "camera"
+    case calendar = "calendar"
+    case contacts = "contacts"
+    case reminders = "reminders"
+    case microphone = "microphone"
+    case photoLibrary = "photo library"
+    case notification = "notification"
+    case mediaLibrary = "media library"
+    case locationWhenInUse = "location when use"
+    case locationAlwaysAndWhenInUse = "location always"
+    
+    public var isAuthorized: Bool {
+        SPPermission.manager(for: self).isAuthorized
     }
-}
-
-public enum SPPermissionMode {
     
-    case native
-    case dialog
-    case fullscreen
+    public var isDenied: Bool {
+        SPPermission.manager(for: self).isDenied
+    }
+    
+    var description: String {
+        return rawValue.localizedUppercase
+    }
+    
+    var usageDescriptionKey: String? {
+        switch self {
+        case .camera:
+            return "NSCameraUsageDescription"
+        case .photoLibrary:
+            return "NSPhotoLibraryUsageDescription"
+        case .notification:
+            return nil
+        case .microphone:
+            return "NSMicrophoneUsageDescription"
+        case .calendar:
+            return "NSCalendarsUsageDescription"
+        case .contacts:
+            return "NSContactsUsageDescription"
+        case .reminders:
+            return "NSRemindersUsageDescription"
+        case .speech:
+            return "NSSpeechRecognitionUsageDescription"
+        case .locationWhenInUse:
+            return "NSLocationWhenInUseUsageDescription"
+        case .locationAlwaysAndWhenInUse:
+            return "NSLocationAlwaysAndWhenInUseUsageDescription"
+        case .motion:
+            return "NSMotionUsageDescription"
+        case .mediaLibrary:
+            return "NSAppleMusicUsageDescription"
+        }
+    }
+    
+    fileprivate static func manager(for permission: SPPermission) -> SPPermissionInterface {
+        switch permission {
+        case .camera:
+            #if SPPERMISSION_CAMERA
+                return SPCameraPermission()
+            #else
+                fatalError(error(permission))
+            #endif
+        case .photoLibrary:
+            #if SPPERMISSION_PHOTOLIBRARY
+                return SPPhotoLibraryPermission()
+            #else
+                fatalError(error(permission))
+            #endif
+        case .notification:
+            #if SPPERMISSION_NOTIFICATION
+                return SPNotificationPermission()
+            #else
+                fatalError(error(permission))
+            #endif
+        case .microphone:
+            #if SPPERMISSION_MICROPHONE
+                return SPMicrophonePermission()
+            #else
+                fatalError(error(permission))
+            #endif
+        case .calendar:
+            #if SPPERMISSION_CALENDAR
+                return SPCalendarPermission()
+            #else
+                fatalError(error(permission))
+            #endif
+        case .contacts:
+            #if SPPERMISSION_CONTACTS
+                return SPContactsPermission()
+            #else
+                fatalError(error(permission))
+            #endif
+        case .reminders:
+            #if SPPERMISSION_REMINDERS
+                return SPRemindersPermission()
+            #else
+                fatalError(error(permission))
+            #endif
+        case .speech:
+            #if SPPERMISSION_SPEECH
+                return SPSpeechPermission()
+            #else
+                fatalError(error(permission))
+            #endif
+        case .locationWhenInUse:
+            #if SPPERMISSION_LOCATION
+                return SPLocationPermission(type: SPLocationPermission.SPLocationType.WhenInUse)
+            #else
+                fatalError(error(permission))
+            #endif
+        case .locationAlwaysAndWhenInUse:
+            #if SPPERMISSION_LOCATION
+                return SPLocationPermission(type: SPLocationPermission.SPLocationType.AlwaysAndWhenInUse)
+            #else
+                fatalError(error(permission))
+            #endif
+        case .motion:
+            #if SPPERMISSION_MOTION
+                return SPMotionPermission()
+            #else
+                fatalError(error(permission))
+            #endif
+        case .mediaLibrary:
+            #if SPPERMISSION_MEDIALIBRARY
+                return SPMediaLibraryPermission()
+            #else
+                fatalError(error(permission))
+            #endif
+        }
+    }
+    
+    fileprivate static func error(_ permission: SPPermission) -> String {
+        return "SPPermissions - \(permission.description) not import. See Readme: https://github.com/ivanvorobei/SPPermission"
+    }
 }
