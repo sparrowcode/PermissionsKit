3cd17b3955da698fa0563de5628c2ce434322bae
Ivan Vorobei <ivanvorobei@icloud.com>
2019-11-12 00:38:24 +0300

    Add section about localization

--- Source/Supporting Files/SPPermissions.h
+++ Source/Supporting Files/SPPermissions.h
@@ -30,3 +30,6 @@ FOUNDATION_EXPORT const unsigned char SPPermissionsVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <SPPermissions/PublicHeader.h>
 
 
+
+
+
