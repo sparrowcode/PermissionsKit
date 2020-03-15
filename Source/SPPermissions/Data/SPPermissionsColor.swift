961c31356bc23e5f83aceaf16bff4450a44a2398
Ivan Vorobei <hello@ivanvorobei.io>
2020-03-16 00:17:27 +0300

    Update email

--- Source/SPPermissions/Data/SPPermissionsColor.swift
+++ Source/SPPermissions/Data/SPPermissionsColor.swift
@@ -1,5 +1,5 @@
 // The MIT License (MIT)
-// Copyright © 2019 Ivan Vorobei (ivanvorobei@icloud.com)
+// Copyright © 2019 Ivan Varabei (varabeis@icloud.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
