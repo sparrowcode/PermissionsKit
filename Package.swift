551a081eff3b1a2a620c9301b1819bd2b877c035
Ivan Vorobei <hello@ivanvorobei.io>
2021-05-10 09:39:51 +0300

    Try split modules with spm.

--- Package.swift
+++ Package.swift
@@ -5,12 +5,24 @@ import PackageDescription
 let package = Package(
     name: "SPPermissions",
     platforms: [
-       .iOS(.v11), .tvOS(.v11)
+        .iOS(.v11), .tvOS(.v11)
     ],
     products: [
-        .library(name: "SPPermissions", targets: ["SPPermissions"])
+        .library(name: "Notifications", targets: ["Notifications"]),
+        .library(name: "PhotoLibrary", targets: ["PhotoLibrary"])
     ],
     targets: [
-        .target(name: "SPPermissions")
+        .target(
+            name: "Notifications",
+            cSettings: [
+                .define("SPPERMISSIONS_NOTIFICATIONS", to: "1")
+            ]
+        )
+        .target(
+            name: "PhotoLibrary",
+            cSettings: [
+                .define("SPPERMISSIONS_PHOTOLIBRARY", to: "1")
+            ]
+        )
     ]
 )
