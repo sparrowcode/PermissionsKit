0e4aaacff7d2abfd07adec6d37ead51583e23405
Ivan Vorobei <hello@ivanvorobei.io>
2021-05-18 10:27:16 +0300

    Updated AGREEMENTS. Added docs.

--- Sources/SPPermissions/SPPermissions.swift
+++ Sources/SPPermissions/SPPermissions.swift
@@ -25,25 +25,48 @@ public enum SPPermissions {
     
     // MARK: - Present Styles
     
+    /**
+     SPPermissions: Request permissions with native style without any special controller.
+     
+     - parameter permissions: List if permissions for request, using model `SPPermissions.Permission`.
+     */
     public static func native(_ permissions: [SPPermissions.Permission]) -> SPPermissionsNativeController {
         let controller = SPPermissionsNativeController(permissions.removedDuplicates())
         return controller
     }
     
     #if os(iOS)
+    
+    /**
+     SPPermissions: Request permissions as list style.
+     
+     - parameter permissions: List if permissions for request, using model `SPPermissions.Permission`.
+     */
     public static func list(_ permissions: [SPPermissions.Permission]) -> SPPermissionsListController {
         let controller = SPPermissionsListController(permissions.removedDuplicates())
         return controller
     }
     
+    /**
+     SPPermissions: Request permissions as dialog style.
+     
+     - parameter permissions: List if permissions for request, using model `SPPermissions.Permission`.
+     */
     public static func dialog(_ permissions: [SPPermissions.Permission]) -> SPPermissionsDialogController {
         let controller = SPPermissionsDialogController(permissions.removedDuplicates())
         return controller
     }
+    
     #endif
     
     // MARK: - Models
     
+    /**
+     SPPermissions: Basic permission model.
+     
+     Using like enum for list of permissions.
+     All permissions will be adding to this class like static.
+     */
     open class Permission: Equatable {
         
         public final var authorized: Bool {
@@ -58,28 +81,50 @@ public enum SPPermissions {
             return status == .notDetermined
         }
         
+        /**
+         SPPermissions: Internal name for permissions.
+         
+         Don't use it in your interface. Only for console debug.
+         */
         public var debugName: String {
             return type.name
         }
         
+        /**
+         SPPermissions: Localized name of permission.
+         
+         Allowed using in interface.
+         */
         public var localisedName: String {
             return Text.permission_name(type)
         }
         
         // MARK: Must Ovveride
         
+        /**
+         SPPermissions: Permission type.
+         */
         open var usageDescriptionKey: String? {
             preconditionFailure("This method must be overridden.")
         }
         
+        /**
+         SPPermissions: Permission type.
+         */
         open var type: PermissionType {
             preconditionFailure("This method must be overridden.")
         }
         
+        /**
+         SPPermissions: Authorization status of permission.
+         */
         open var status: PermissionStatus {
             preconditionFailure("This method must be overridden.")
         }
         
+        /**
+         SPPermissions: Call request of permission in system.
+         */
         open func request(completion: @escaping ()->Void) {
             preconditionFailure("This method must be overridden.")
         }
@@ -93,6 +138,9 @@ public enum SPPermissions {
         public init() {}
     }
     
+    /**
+     SPPermissions: Permissions authorization status.
+     */
     @objc public enum PermissionStatus: Int {
         
         case authorized
@@ -100,6 +148,11 @@ public enum SPPermissions {
         case notDetermined
     }
     
+    /**
+     SPPermissions: Permissions type.
+     
+     Type using like identifier for each permission.
+     */
     @objc public enum PermissionType: Int {
         
         case camera = 0
