e38ab0a73547d876d1cce1db4ba092eed3f87736
Ivan Vorobei <hello@ivanvorobei.io>
2021-07-26 12:31:48 +0300

    Updated tvOS example target.

--- Example Apps/tvOS Example/App/AppDelegate.swift
+++ Example Apps/tvOS Example/App/AppDelegate.swift
@@ -23,19 +23,11 @@ import UIKit
 import SparrowKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: SPAppWindowDelegate {
 
-    var window: UIWindow?
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        launch(RootController().wrapToNavigationController(prefersLargeTitles: true))
+        let rootController = RootController().wrapToNavigationController(prefersLargeTitles: true)
+        makeKeyAndVisible(viewController: rootController, tint: .systemBlue)
         return true
-    }
-    
-    func launch(_ viewController: UIViewController) {
-        let frame = UIScreen.main.bounds
-        window = UIWindow(frame: frame)
-        window?.rootViewController = viewController
-        window?.makeKeyAndVisible()
     }
 }
